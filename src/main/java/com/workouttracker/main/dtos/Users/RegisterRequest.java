9dd7bc7bd64a41f881555521026fbc7ed30be458
Darenz Jasper A. Hicap <hicap.darenzjasper@gmail.com>
2025-10-30 10:18:14 +0800

    feat: Add password confirmation and validation to registration process

--- src/main/java/com/workouttracker/main/dtos/Users/RegisterRequest.java
+++ src/main/java/com/workouttracker/main/dtos/Users/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.workouttracker.main.dtos.Users;
 
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -36,4 +37,12 @@ public class RegisterRequest {
     @NotBlank(message = "Password is required")
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character")
     private String password;
+
+    @NotBlank(message = "Please confirm your password")
+    private String confirmPassword;
+
+    @AssertTrue(message = "Passwords do not match")
+    public boolean isPasswordConfirmed() {
+        return password != null && password.equals(confirmPassword);
+    }
 }
