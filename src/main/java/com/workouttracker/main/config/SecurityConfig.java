24f8bc8e062adac5d963656951ee90bbf0a1eb2f
Darenz Jasper A. Hicap <hicap.darenzjasper@gmail.com>
2025-10-31 11:51:09 +0800

    feat: Enhance security and user experience by updating CSRF handling and adding CSRF token to forms

--- src/main/java/com/workouttracker/main/config/SecurityConfig.java
+++ src/main/java/com/workouttracker/main/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
 
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-                http.csrf(csrf -> csrf.disable());
+                http.csrf(csrf -> csrf.ignoringRequestMatchers("/logout"));
 
                 http.authorizeHttpRequests(auth -> auth
                                 .requestMatchers("/login", "/register", "/logout", "/api/validate/**")
